1e5e05325952969ba6cbec79a53bb8a0f90f50af
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-05-16 23:49:06 -0400

    Updates

---
+++ precache-manifest.fae67f0497612f53fe186c77700abd4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "109107e5be9f5948871a",
+    "url": "/javascript-miniprojects/static/css/main.1e1edaa2.chunk.css"
+  },
+  {
+    "revision": "109107e5be9f5948871a",
+    "url": "/javascript-miniprojects/static/js/main.109107e5.chunk.js"
+  },
+  {
+    "revision": "917f068c31674d5dcb4f",
+    "url": "/javascript-miniprojects/static/js/1.917f068c.chunk.js"
+  },
+  {
+    "revision": "2273cb4af804d7ecae19",
+    "url": "/javascript-miniprojects/static/js/runtime~main.2273cb4a.js"
+  },
+  {
+    "revision": "db4c16ad73a6cf23755cbd9fcbabc742",
+    "url": "/javascript-miniprojects/index.html"
+  }
+];
