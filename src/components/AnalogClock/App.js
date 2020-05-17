94e07b0f83a704079be5d89bf2042c79e362bf09
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-05-16 21:01:01 -0400

    fix: implemented useEffect for loading once

--- src/components/AnalogClock/App.js
+++ src/components/AnalogClock/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React , { useEffect }from 'react';
 
 // import './script'
 import './styles.css'
 
+
 export default function App() {
-    // const script = require('./script')
+
+    useEffect( () => {
+    setInterval(setClock, 1000)
+
+    const hourHand = document.querySelector('[data-hour-hand')
+    const minuteHand = document.querySelector('[data-minute-hand')
+    const secondHand = document.querySelector('[data-second-hand')
+
+    function setClock() {
+        const currentDate = new Date()
+        const secondsRatio = currentDate.getSeconds() / 60
+        const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+        const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+        setRotation(secondHand, secondsRatio)
+        setRotation(minuteHand, minutesRatio)
+        setRotation(hourHand, hoursRatio)
+    }
+
+    function setRotation(element, rotationRatio) {
+        element.style.setProperty('--rotation', rotationRatio * 360)
+    }
+
+    setClock()}, [] )
+
     return(
         <section class="clock-body">
         <div class="clock">
